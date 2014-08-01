02d0f28d9de3faf46cf5b11a825be87ee121c6ba
Justin Lloyd <justin@justinlloyd.org>
2014-08-01 00:00:00 -0800

    Show that the status message edit text event is being handled
    Add a log message to the onTextChanged event handler for the status message edit text that shows the text being entered by the user

--- app/src/main/java/org/justinlloyd/yambaclient/PostStatusUpdate.java
+++ app/src/main/java/org/justinlloyd/yambaclient/PostStatusUpdate.java
@@ -22,12 +22,12 @@ public class PostStatusUpdate extends Activity {
         editTextStatusMessage.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                
+
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-
+                Log.d(PostStatusUpdate.class.getName(), "Text changed: \"" + s.toString() + "\"");
             }
 
             @Override
