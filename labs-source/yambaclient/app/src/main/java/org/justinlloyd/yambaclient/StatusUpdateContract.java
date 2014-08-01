3b03ee094edd464b3fde8e50b13604b936c30d77
Justin Lloyd <justin@justinlloyd.org>
2014-08-01 00:00:00 -0800

    Define named constant strings for the Status types to prevent future typos

--- labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/StatusUpdateContract.java
+++ labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/StatusUpdateContract.java
@@ -11,6 +11,8 @@ public class StatusUpdateContract
 	public static final String AUTHORITY = "org.justinlloyd.yambaclient.StatusUpdateProvider";
 	public static final int STATUS_UPDATE_ITEM = 1;
 	public static final int STATUS_UPDATE_DIR = 2;
+	public static final String STATUS_TYPE_ITEM = "vnd.android.cursor.item/vnd.org.justinlloyd.yambaclient.provider.status";
+	public static final String STATUS_TYPE_DIR = "vnd.android.cursor.dir/vnd.org.justinlloyd.yambaclient.provider.status";
 
 	public class DataColumn
 	{
