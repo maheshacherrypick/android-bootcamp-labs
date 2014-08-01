80944b6d01b77ab5ad7929b4b458a6d27b5e6ecc
Justin Lloyd <justin@justinlloyd.org>
2014-08-01 00:00:00 -0800

    Declare named constants that will map URIs to numbered actions

--- labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/StatusUpdateContract.java
+++ labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/StatusUpdateContract.java
@@ -9,6 +9,8 @@ public class StatusUpdateContract
 	public static final String TABLE_NAME = "status";
 
 	public static final String AUTHORITY = "org.justinlloyd.yambaclient.StatusUpdateProvider";
+	public static final int STATUS_UPDATE_ITEM = 1;
+	public static final int STATUS_UPDATE_DIR = 2;
 
 	public class DataColumn
 	{
