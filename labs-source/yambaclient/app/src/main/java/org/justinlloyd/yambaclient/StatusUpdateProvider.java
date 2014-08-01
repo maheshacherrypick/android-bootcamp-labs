18adf74a5fc1139cdb991f7d59acff52abd664d9
Justin Lloyd <justin@justinlloyd.org>
2014-08-01 00:00:00 -0800

    Reformat code for consistency

--- labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/StatusUpdateProvider.java
+++ labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/StatusUpdateProvider.java
@@ -7,7 +7,8 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.util.Log;
 
-public class StatusUpdateProvider extends ContentProvider {
+public class StatusUpdateProvider extends ContentProvider
+{
 	private static final String TAG = StatusUpdateProvider.class.getName();
 
 	private static final UriMatcher sURIMatcher = new UriMatcher(UriMatcher.NO_MATCH);
@@ -18,17 +19,20 @@ public class StatusUpdateProvider extends ContentProvider {
 		sURIMatcher.addURI(StatusUpdateContract.AUTHORITY, StatusUpdateContract.TABLE_NAME + "/#", StatusUpdateContract.STATUS_UPDATE_ITEM);
 	}
 
-    public StatusUpdateProvider() {
-    }
+	public StatusUpdateProvider()
+	{
+	}
 
-    @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
-        // Implement this to handle requests to delete one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+	@Override
+	public int delete(Uri uri, String selection, String[] selectionArgs)
+	{
+		// Implement this to handle requests to delete one or more rows.
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
 
-    @Override
-    public String getType(Uri uri) {
+	@Override
+	public String getType(Uri uri)
+	{
 		switch (sURIMatcher.match(uri))
 		{
 			case StatusUpdateContract.STATUS_UPDATE_DIR:
@@ -41,31 +45,35 @@ public class StatusUpdateProvider extends ContentProvider {
 				Log.e(TAG, "Illegal URI supplied: " + uri);
 				throw new IllegalArgumentException("Illegal URI supplied: " + uri);
 		}
-    }
+	}
 
-    @Override
-    public Uri insert(Uri uri, ContentValues values) {
-        // TODO: Implement this to handle requests to insert a new row.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+	@Override
+	public Uri insert(Uri uri, ContentValues values)
+	{
+		// TODO: Implement this to handle requests to insert a new row.
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
 
-    @Override
-    public boolean onCreate() {
-        // TODO: Implement this to initialize your content provider on startup.
-        return false;
-    }
+	@Override
+	public boolean onCreate()
+	{
+		// TODO: Implement this to initialize your content provider on startup.
+		return false;
+	}
 
-    @Override
-    public Cursor query(Uri uri, String[] projection, String selection,
-            String[] selectionArgs, String sortOrder) {
-        // TODO: Implement this to handle query requests from clients.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection,
+						String[] selectionArgs, String sortOrder)
+	{
+		// TODO: Implement this to handle query requests from clients.
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
 
-    @Override
-    public int update(Uri uri, ContentValues values, String selection,
-            String[] selectionArgs) {
-        // TODO: Implement this to handle requests to update one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+	@Override
+	public int update(Uri uri, ContentValues values, String selection,
+					  String[] selectionArgs)
+	{
+		// TODO: Implement this to handle requests to update one or more rows.
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
 }
