627a05ffefb232ca7f83f39c6578b3635d7d4ef4
Justin Lloyd <justin@justinlloyd.org>
2014-08-01 00:00:00 -0800

    Remove redundant template code in insert function

--- labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/StatusUpdateProvider.java
+++ labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/StatusUpdateProvider.java
@@ -50,8 +50,6 @@ public class StatusUpdateProvider extends ContentProvider
 	@Override
 	public Uri insert(Uri uri, ContentValues values)
 	{
-		// TODO: Implement this to handle requests to insert a new row.
-		throw new UnsupportedOperationException("Not yet implemented");
 	}
 
 	@Override
