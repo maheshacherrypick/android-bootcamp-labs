98a7d12ad4cf94102531469bfe40ca3f7fb0141c
Justin Lloyd <justin@justinlloyd.org>
2014-08-01 00:00:00 -0800

    Use the new string resource with parameterisation instead of the hard-coded string

--- app/src/main/java/org/justinlloyd/yambaclient/PostStatusUpdate.java
+++ app/src/main/java/org/justinlloyd/yambaclient/PostStatusUpdate.java
@@ -100,7 +100,9 @@ public class PostStatusUpdate extends Activity {
             long endTime = System.currentTimeMillis();
             long totalTime = endTime - startTime;
             Log.d(TAG, String.format("Posted the status message in %d ms", totalTime));
-            Toast.makeText(this, String.format("Posted the status message in %d ms", totalTime), Toast.LENGTH_SHORT).show();
+
+            String text = String.format(getResources().getString(R.string.time_to_post), totalTime);
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
         } catch (YambaClientException e) {
             Log.e(TAG, e.toString());
         }
