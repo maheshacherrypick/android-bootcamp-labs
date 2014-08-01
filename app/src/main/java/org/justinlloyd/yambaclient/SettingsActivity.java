cf4d6a16b026a46f50e07874cd9dded86439aefa
Justin Lloyd <justin@justinlloyd.org>
2014-08-01 00:00:00 -0800

    Load the preferences from the resource file
    Make use of the deprecated function to do this for now to keep the example simple

--- app/src/main/java/org/justinlloyd/yambaclient/SettingsActivity.java
+++ app/src/main/java/org/justinlloyd/yambaclient/SettingsActivity.java
@@ -7,6 +7,7 @@ public class SettingsActivity extends PreferenceActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
     }
 }
 
