1807d6d96ab3146d9a501aedcf47b3f7d735be94
Justin Lloyd <justin@justinlloyd.org>
2014-08-01 00:00:00 -0800

    Log a message indicating the repeating alarm has been set and the refresh interval being used

--- labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/BootReceiver.java
+++ labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/BootReceiver.java
@@ -5,6 +5,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class BootReceiver extends BroadcastReceiver
 {
@@ -20,6 +21,8 @@ public class BootReceiver extends BroadcastReceiver
 	{
 		PendingIntent pendingRefresh = PendingIntent.getService(context, -1, new Intent(context, RefreshService.class), PendingIntent.FLAG_UPDATE_CURRENT);
 		AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+		Log.d(TAG, "Set a repeating alarm for the YambaClient RefreshService to " + DEFAULT_REFRESH_INTERVAL + " minutes.");
 		alarmManager.setInexactRepeating(AlarmManager.RTC, System.currentTimeMillis(), DEFAULT_REFRESH_INTERVAL, pendingRefresh);
 	}
 }
