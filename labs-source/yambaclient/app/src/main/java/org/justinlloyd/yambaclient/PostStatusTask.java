5513f8c1b0418612cbbc85bd68246338d38aa932
Justin Lloyd <justin@justinlloyd.org>
2014-08-01 00:00:00 -0800

    Change the username is empty toast to use the string resource

--- labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/PostStatusTask.java
+++ labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/PostStatusTask.java
@@ -87,7 +87,7 @@ public class PostStatusTask extends AsyncTask<String, Void, Long> {
             } else if (result == POST_FAILED) {
                 Toast.makeText(context, context.getResources().getString(R.string.post_status_update_failed), Toast.LENGTH_SHORT).show();
             } else if (result == POST_FAILED_USERNAME_EMPTY) {
-                Toast.makeText(context, "Username is empty.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, context.getResources().getString(R.string.post_status_update_username_empty), Toast.LENGTH_SHORT).show();
             } else if (result == POST_FAILED_PASSWORD_EMPTY) {
                 Toast.makeText(context, "Password is empty.", Toast.LENGTH_SHORT).show();
             } else if (result == POST_FAILED_UNAUTHORIZED) {
