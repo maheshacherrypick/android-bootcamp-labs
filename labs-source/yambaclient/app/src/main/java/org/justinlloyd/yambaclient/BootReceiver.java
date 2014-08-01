72aef27620bb590dea3dbe7186327aa7a156e04f
Justin Lloyd <justin@justinlloyd.org>
2014-08-01 00:00:00 -0800

    Remove boiler plate template code

--- labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/BootReceiver.java
+++ labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/BootReceiver.java
@@ -17,8 +17,5 @@ public class BootReceiver extends BroadcastReceiver
 	@Override
 	public void onReceive(Context context, Intent intent)
 	{
-		// TODO: This method is called when the BroadcastReceiver is receiving
-		// an Intent broadcast.
-		throw new UnsupportedOperationException("Not yet implemented");
 	}
 }
