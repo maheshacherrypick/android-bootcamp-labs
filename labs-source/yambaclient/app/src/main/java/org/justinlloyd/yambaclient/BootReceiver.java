88200349eea8afadea41caf905fccf389fff525e
Justin Lloyd <justin@justinlloyd.org>
2014-08-01 00:00:00 -0800

    Change the alarm manager to use the refresh interval stored in the shared preferences or the default refresh interval if the shared preferences have not been set

--- labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/BootReceiver.java
+++ labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/BootReceiver.java
@@ -27,7 +27,7 @@ public class BootReceiver extends BroadcastReceiver
 		PendingIntent pendingRefresh = PendingIntent.getService(context, -1, new Intent(context, RefreshService.class), PendingIntent.FLAG_UPDATE_CURRENT);
 		AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 
-		Log.d(TAG, "Set a repeating alarm for the YambaClient RefreshService to " + DEFAULT_REFRESH_INTERVAL + " minutes.");
-		alarmManager.setInexactRepeating(AlarmManager.RTC, System.currentTimeMillis(), DEFAULT_REFRESH_INTERVAL, pendingRefresh);
+		Log.d(TAG, "Set a repeating alarm for the YambaClient RefreshService to " + refreshInterval + " minutes.");
+		alarmManager.setInexactRepeating(AlarmManager.RTC, System.currentTimeMillis(), refreshInterval, pendingRefresh);
 	}
 }
