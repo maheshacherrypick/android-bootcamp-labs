0be539d46f0857e014ea472f765141f1f36787b7
Justin Lloyd <justin@justinlloyd.org>
2014-08-01 00:00:00 -0800

    Override the onCreate method

--- app/src/main/java/org/justinlloyd/yambaclient/SettingsActivity.java
+++ app/src/main/java/org/justinlloyd/yambaclient/SettingsActivity.java
@@ -1,7 +1,12 @@
 package org.justinlloyd.yambaclient;
 
+import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 public class SettingsActivity extends PreferenceActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
 
