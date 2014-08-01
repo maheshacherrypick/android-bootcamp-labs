11f5c37b4e55c4d53e6325478c4e5643fb9ee00d
Justin Lloyd <justin@justinlloyd.org>
2014-08-01 00:00:00 -0800

    Reformat source code for consistency

--- labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/RefreshService.java
+++ labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/RefreshService.java
@@ -1,60 +1,67 @@
 package org.justinlloyd.yambaclient;
 
 import android.app.IntentService;
-import android.content.Intent;
 import android.content.Context;
+import android.content.Intent;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
  * a service on a separate handler thread.
- * <p>
+ * <p/>
  * TODO: Customize class - update intent actions, extra parameters and static
  * helper methods.
  */
-public class RefreshService extends IntentService {
-    // TODO: Rename actions, choose action names that describe tasks that this
-    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-    private static final String ACTION_REFRESH = "org.justinlloyd.yambaclient.action.REFRESH";
+public class RefreshService extends IntentService
+{
+	// TODO: Rename actions, choose action names that describe tasks that this
+	// IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+	private static final String ACTION_REFRESH = "org.justinlloyd.yambaclient.action.REFRESH";
 
-    // TODO: Rename parameters
-    private static final String EXTRA_PARAM1 = "org.justinlloyd.yambaclient.extra.ITEMS_TO_RETRIEVE";
+	// TODO: Rename parameters
+	private static final String EXTRA_PARAM1 = "org.justinlloyd.yambaclient.extra.ITEMS_TO_RETRIEVE";
 
-    /**
-     * Starts this service to perform action Refresh with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionRefresh(Context context, String param1) {
-        Intent intent = new Intent(context, RefreshService.class);
-        intent.setAction(ACTION_REFRESH);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        context.startService(intent);
-    }
+	/**
+	 * Starts this service to perform action Refresh with the given parameters. If
+	 * the service is already performing a task this action will be queued.
+	 *
+	 * @see IntentService
+	 */
+	// TODO: Customize helper method
+	public static void startActionRefresh(Context context, String param1)
+	{
+		Intent intent = new Intent(context, RefreshService.class);
+		intent.setAction(ACTION_REFRESH);
+		intent.putExtra(EXTRA_PARAM1, param1);
+		context.startService(intent);
+	}
 
-    public RefreshService() {
-        super("RefreshService");
-    }
+	public RefreshService()
+	{
+		super("RefreshService");
+	}
 
-    @Override
-    protected void onHandleIntent(Intent intent) {
-        if (intent != null) {
-            final String action = intent.getAction();
-            if (ACTION_REFRESH.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                handleActionRefresh(param1);
-            }
-        }
-    }
+	@Override
+	protected void onHandleIntent(Intent intent)
+	{
+		if (intent != null)
+		{
+			final String action = intent.getAction();
+			if (ACTION_REFRESH.equals(action))
+			{
+				final String param1 = intent.getStringExtra(EXTRA_PARAM1);
+				handleActionRefresh(param1);
+			}
+		}
+	}
 
-    /**
-     * Handle action Refresh in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionRefresh(String param1) {
-        // TODO: Handle action Refresh
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+	/**
+	 * Handle action Refresh in the provided background thread with the provided
+	 * parameters.
+	 */
+	private void handleActionRefresh(String param1)
+	{
+		// TODO: Handle action Refresh
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
 
 }
