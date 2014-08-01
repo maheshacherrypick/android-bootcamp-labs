877f4e268f13532ab5dab2d79297a6fca42dd217
Justin Lloyd <justin@justinlloyd.org>
2014-08-01 00:00:00 -0800

    Add default refresh interval for the alarm manager

--- labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/BootReceiver.java
+++ labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/BootReceiver.java
@@ -1,5 +1,6 @@
 package org.justinlloyd.yambaclient;
 
+import android.app.AlarmManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -7,6 +8,7 @@ import android.content.Intent;
 public class BootReceiver extends BroadcastReceiver
 {
 	private static final String TAG = BootReceiver.class.getName();
+	private static final long DEFAULT_REFRESH_INTERVAL = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
 
 	public BootReceiver()
 	{
