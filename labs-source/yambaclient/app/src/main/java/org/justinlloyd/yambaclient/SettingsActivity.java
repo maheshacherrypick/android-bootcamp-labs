6a5bf4e0d45edec8c4836aa65cf1dab685bdf102
Justin Lloyd <justin@justinlloyd.org>
2014-08-01 00:00:00 -0800

    Reformat source code for consistency

--- labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/SettingsActivity.java
+++ labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/SettingsActivity.java
@@ -7,27 +7,32 @@ import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 
-public class SettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.settings);
-    }
+public class SettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.settings);
+	}
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
-    }
+	@Override
+	public void onStart()
+	{
+		super.onStart();
+		PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
+	}
 
-    @Override
-    public void onStop() {
-        super.onStop();
-        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this);
-    }
+	@Override
+	public void onStop()
+	{
+		super.onStop();
+		PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this);
+	}
 
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
+	{
 		sendBroadcast(new Intent("org.justinlloyd.yambaclient.action.UPDATED_INTERVAL"));
 	}
 }
