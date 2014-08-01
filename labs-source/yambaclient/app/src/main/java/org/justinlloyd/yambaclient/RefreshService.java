74c8649d376d76ea8bbcea0b16ed92e981b75a15
Justin Lloyd <justin@justinlloyd.org>
2014-08-01 00:00:00 -0800

    Refresh all references to "Baz" in auto-generated service template class

--- labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/RefreshService.java
+++ labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/RefreshService.java
@@ -15,7 +15,6 @@ public class RefreshService extends IntentService {
     // TODO: Rename actions, choose action names that describe tasks that this
     // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
     private static final String ACTION_REFRESH = "org.justinlloyd.yambaclient.action.FOO";
-    private static final String ACTION_BAZ = "org.justinlloyd.yambaclient.action.BAZ";
 
     // TODO: Rename parameters
     private static final String EXTRA_PARAM1 = "org.justinlloyd.yambaclient.extra.PARAM1";
@@ -36,21 +35,6 @@ public class RefreshService extends IntentService {
         context.startService(intent);
     }
 
-    /**
-     * Starts this service to perform action Baz with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionBaz(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, RefreshService.class);
-        intent.setAction(ACTION_BAZ);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
-
     public RefreshService() {
         super("RefreshService");
     }
@@ -63,10 +47,6 @@ public class RefreshService extends IntentService {
                 final String param1 = intent.getStringExtra(EXTRA_PARAM1);
                 final String param2 = intent.getStringExtra(EXTRA_PARAM2);
                 handleActionRefresh(param1, param2);
-            } else if (ACTION_BAZ.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionBaz(param1, param2);
             }
         }
     }
@@ -80,12 +60,4 @@ public class RefreshService extends IntentService {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    /**
-     * Handle action Baz in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionBaz(String param1, String param2) {
-        // TODO: Handle action Baz
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
 }
