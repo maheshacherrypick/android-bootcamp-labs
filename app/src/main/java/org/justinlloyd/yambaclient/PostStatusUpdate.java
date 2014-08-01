4b26722a3299696673e65e37a1bb67fe3bef11f1
Justin Lloyd <justin@justinlloyd.org>
2014-08-01 00:00:00 -0800

    Retrieve the password from the shared preferences and log it

--- app/src/main/java/org/justinlloyd/yambaclient/PostStatusUpdate.java
+++ app/src/main/java/org/justinlloyd/yambaclient/PostStatusUpdate.java
@@ -166,6 +166,8 @@ public class PostStatusUpdate extends Activity {
             YambaClient yc = new YambaClient("student", "password");
             String username = PreferenceManager.getDefaultSharedPreferences(context).getString("username", "student");
             Log.d(TAG, String.format("Username is set to: \"%s\"", username));
+            String password = PreferenceManager.getDefaultSharedPreferences(context).getString("password", "password");
+            Log.d(TAG, String.format("Password is set to: \"%s\"", password));
             try {
                 long startTime = System.currentTimeMillis();
                 yc.postStatus(params[0]);
