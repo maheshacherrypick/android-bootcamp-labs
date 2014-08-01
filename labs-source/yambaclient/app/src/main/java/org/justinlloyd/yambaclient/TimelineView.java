49b91846d5b53ea39edb5a7a25aa2db002678ae9
Justin Lloyd <justin@justinlloyd.org>
2014-08-01 00:00:00 -0800

    Launch Settings Activity when the settings action is clicked on the Timeline View activity

--- labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/TimelineView.java
+++ labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/TimelineView.java
@@ -45,6 +45,7 @@ public class TimelineView extends Activity
 		int id = item.getItemId();
 		if (id == R.id.action_settings)
 		{
+			startActivity(new Intent(this, SettingsActivity.class));
 			return true;
 		}
 		else if (id == R.id.action_refresh)
