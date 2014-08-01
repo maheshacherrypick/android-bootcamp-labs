74b77d0463f301ff5097a1ebfd991999428430c4
Justin Lloyd <justin@justinlloyd.org>
2014-08-01 00:00:00 -0800

    Remove redundant Settings action item code

--- labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/TimelineView.java
+++ labs-source/yambaclient/app/src/main/java/org/justinlloyd/yambaclient/TimelineView.java
@@ -42,12 +42,6 @@ public class TimelineView extends Activity
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings)
-		{
-			startActivity(new Intent(this, SettingsActivity.class));
-			return true;
-		}
 
 		return super.onOptionsItemSelected(item);
 	}
